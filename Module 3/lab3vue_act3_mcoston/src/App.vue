5ffdb5bbd736385a9787d8d5d3ea878f8bb68852
mcoston1 <115651873+mcoston1@users.noreply.github.com>
2023-09-13 16:49:46 -0400

    attempt activity3

---
+++ Module 3/lab3vue_act3_mcoston/src/App.vue
@@ -0,0 +1,59 @@
+<script>
+import Balance from './components/Balance.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Currency from './components/Currency.vue'
+import { reactive } from 'vue'
+
+export default reactive({
+  data() {
+    return {
+      fname: "Miranda",
+      bal: 0,
+    }
+  },
+  components: {
+    Balance,
+    HelloWorld,
+    Currency
+  }
+})
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld :name="this.fname" /> 
+      <Balance @new-bal="(v) => this.bal=v" />
+      <Currency :amount= "this.bal" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: left;
+    padding-right: calc(var(--section-gap) / 4);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ Module 3/lab3vue_act3_mcoston/src/components/Balance.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+    name: 'Balance',
+    
+    data() {
+        return {
+            balance: 100,
+            chgamount: 10
+        }
+    },
+    emits: ['new-bal'], 
+    computed: {
+        balanceString() {
+            return this.balance;
+        },
+    },
+    methods: {
+        addBalance() {
+            this.balance += this.chgamount
+            this.$emit('new-bal', this.balance);
+            //this.amount = 0;
+        },
+        subtractBalance() {
+            this.balance -= this.chgamount
+            this.$emit('new-bal', this.balance);
+            //this.amount = 0;
+        }
+    },
+    mounted() {
+        console.log('Application mounted');
+    },
+}
+</script>
+
+<template>
+    <div class="greetings">
+      <span>Account Balance: {{balanceString}}, Amount: {{chgamount}}</span> <br/>
+      <label for="slider">Change amount to: </label>
+      <input type="range" id="slider" v-model.number="chgamount" min="5" max="100" step="5"> <br/>
+      <button @click="addBalance">Add</button>
+      <button v-if="chgamount < balance" @click="subtractBalance">Subtract</button>
+    </div>
+  </template>
+  
+  <style scoped>
+  h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    top: -10px;
+  }
+  
+  h3 {
+    font-size: 1.2rem;
+  }
+  
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+  
+  @media (min-width: 1024px) {
+    .greetings h1,
+    .greetings h3 {
+      text-align: left;
+    }
+  }
+  </style>
