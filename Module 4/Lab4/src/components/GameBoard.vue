614b60820019747d49e80a39118350b8b8016687
Miranda Coston <mcoston@its.work>
2023-09-20 14:35:22 -0400

    more l4 work

--- Module 4/Lab4/src/components/GameBoard.vue
+++ Module 4/Lab4/src/components/GameBoard.vue
@@ -2,8 +2,25 @@
     export default {
         data() {
             return {
-
+                currentplayer:1,
+                player1score:0,
+                player2score:0,
+                player3score:0,
+                currentcat:'',
+                currentquest:'',
+                hasstarted:false
             }
+        },
+        computed: {
+
+        },    
+        methods: {
+          category(number) {
+          //API call here to get category name
+          },
+          cat2() {
+          //api call here to get category 2 name
+          },
         }
     }
 </script>
@@ -11,18 +28,17 @@
 <template>
     <div id="gameboard">
         <span class="playerscores">
-            <div id="player1score">Player 1: </div> 
-            <div id="player2score">Player 2: </div> 
-            <div id="player3score">Player 3: </div>
+            <div id="player1score">Player 1: {{player1score}}</div> 
+            <div id="player2score">Player 2: {{player2score}}</div> 
+            <div id="player3score">Player 3: {{player3score}}</div>
         </span>
-        <br/>
         <table id="board">   
-            <caption style="color:black"> Jeopardy!</caption>     
+            <caption style="color:black; font-weight: bold;"> Jeopardy!</caption>     
             <tr class="boardrow">
-                <th id="category1">{{cat1}}</th>
-                <th id="category2">{{cat2}}</th>
-                <th id="category3">{{cat3}}</th>
-                <th id="category4">{{cat4}}</th>
+                <th id="category1">{{category(1)}}</th>
+                <th id="category2">{{category(2)}}</th>
+                <th id="category3">{{category(3)}}</th>
+                <th id="category4">{{category(4)}}</th>
             </tr>
             <tr>
                 <td>$100</td>
@@ -55,30 +71,44 @@
                 <td>$500</td>
             </tr>
         </table> 
-        <br/>
-        <h4>Category: </h4> <br/>
-        <h4>Value: </h4> <br/>
+        <h4>Category: {{currentcat}}</h4>
+        <h4>Value: {{currentquest}}</h4>
         <h4>Question: </h4> <br/>
         <div><button>True</button> <button>False</button></div>
     </div>
 </template>
 
 <style>
-#gameboard {
-    
-}
-table, th, td {
+table, td {
   border: 2px solid black;
+  border-collapse: collapse;
+  padding: 15px;
   font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
   font-size:x-large;
   background-color: blue;
-  color:yellow;
-  
+  color:yellow;  
 }
+
+th {
+  border: 2px solid black;
+  border-collapse: collapse;
+  padding: 15px;
+  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size:x-large;
+  background-color: blue;
+  color:white;
+}
+
 .playerscores {
     font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     font-size:large;
 }
 
+.correct {
+    color:green
+}
+.incorrect {
+    color:red
+}
 
 </style>
